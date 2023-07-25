5d9135259a6e5e9d0897cdb3ddaf6bb25b4bbcdf
R4CE1962 <121911052+R4CE1962@users.noreply.github.com>
2023-07-25 19:34:52 -0400

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,21 @@
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    window.location = "kwitter_room.html";
+}
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBDmpIWJ57U5k8NSfmxSi_iFMGLqpbI-sk",
+    authDomain: "lets-chat-web-app-21757.firebaseapp.com",
+    projectId: "lets-chat-web-app-21757",
+    storageBucket: "lets-chat-web-app-21757.appspot.com",
+    messagingSenderId: "972517762149",
+    appId: "1:972517762149:web:6cda3bd288ae30bb78d28c",
+    measurementId: "G-967793ZK9E"
+  };
+
+  firebaseConfig.initializeApp(firebaseConfig);
+
+  function add()
+
